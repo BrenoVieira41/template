67937cb56f63bec6e25bed3aa8449294f0b0cb58
Breno Henrique Vieira <brenohvl@hotmail.com>
2022-09-14 14:38:48 -0300

    fix: repaer template

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import UserRoutes from './resources/User/routes';
-import AuthRoutes from './resources/Auth/routes';
 
 const app = express();
-app.use(express.json());
-app.use(UserRoutes);
-app.use(AuthRoutes);
+
+  app.use(express.json());
 
 export { app };
